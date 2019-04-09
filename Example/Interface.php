bb06c1e05c0ab2059864d53cb1606eb4630c0181
Martin Dobry <32479112+dobraczek@users.noreply.github.com>
2019-04-09 18:47:10 +0200

    Create Interface.php

---
+++ Example/Interface.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<title>Mikrotik API - Interface</title>
+	
+	<meta content="" name="keywords">
+	<meta content="" name="author">
+	
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+
+<h1>Mikrotik API - Interface</h1>
+
+<?php
+
+include "./../Router.php";
+$RouterApi = new MikrotikApi\Router();
+
+if ($RouterApi->connect('192.168.0.1', 'user', 'password'))
+{
+   $RouterApi->write('/interface/getall');
+   
+   $data = $RouterApi->read(false);
+   $array = $RouterApi->parseResponse($data);
+   
+   echo '<pre>'.print_r($array, 1).'</pre>';
+  
+   $RouterApi->disconnect();
+}
+
+?>
+
+</body>
+</html>
